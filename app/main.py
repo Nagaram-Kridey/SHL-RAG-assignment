9afc240baa35f36ca3770add7b32aa17cd4d57ac
Nagaram-Kridey <110899289+Nagaram-Kridey@users.noreply.github.com>
2025-04-07 15:20:33 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import json
 from pathlib import Path
 from app.recommender import recommend_query
+from app.dataloader_2 import load_data_and_update_index
 
 app = FastAPI()
 DATA_PATH = Path("data/shl_assessments.json")
